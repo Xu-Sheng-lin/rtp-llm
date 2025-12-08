212a6f77aabcd5ee3842534b569151aa86ff9cd7
曲青 <xushenglin.xsl@alibaba-inc.com>
2025-12-08 19:23:24 +0800

    refactor rocm_mla_attention_test.py

--- rtp_llm/models_py/test/rocm_mla_attention_test.py
+++ rtp_llm/models_py/test/rocm_mla_attention_test.py
@@ -19,12 +19,12 @@ from rtp_llm.models.rotary_embedding.deepseek_rotary_embedding import (
 from rtp_llm.models_py.modules.common.mla.mla_attention import MlaAttention
 from rtp_llm.models_py.modules.common.mla.mla_attention_ref import MlaAttentionRef
 from rtp_llm.models_py.modules.rocm.mla.mla_attention_ops import (
-    AiterMlaPrefillImpl
+    AiterMlaDecodeImpl, AiterMlaPrefillImpl
 )
-
 from rtp_llm.ops.compute_ops import KVCache, PyAttentionInputs
 from rtp_llm.utils.model_weight import W
 
+from aiter import dtypes
 
 def set_seed(seed: int):
     random.seed(seed)
@@ -32,23 +32,83 @@ def set_seed(seed: int):
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+# 以下是从test_pa.py移植过来的test_paged_attention函数及其依赖代码
 
-def generate_kv_from_cache(ckv, kpe, kv_len, batch_size, num_heads):
-    bs_page_num, page_size, ckv_dim = ckv.shape
-    page_num = bs_page_num // batch_size
-    _, _, kpe_dim = kpe.shape
-    ckv = ckv.view(batch_size, page_num * page_size, ckv_dim)
-    kpe = kpe.view(batch_size, page_num * page_size, kpe_dim)
-    ckv = ckv[:, :kv_len, :]
-    kpe = kpe[:, :kv_len, :]
-    k = (
-        torch.cat([ckv, kpe], dim=-1)
-        .view(-1, 1, ckv_dim + kpe_dim)
-        .repeat_interleave(num_heads, dim=1)
-    )
-    v = ckv.repeat_interleave(num_heads, dim=1)
+uniform_range = (-1, 1)
+STR_DTYPE_TO_TORCH_DTYPE = {
+    "half": torch.half,
+    "bfloat16": dtypes.bf16,
+    "float": dtypes.fp32,
+    "fp8": torch.uint8,
+    "fp8_e4m3": torch.uint8,
+    "fp8_e5m2": torch.uint8,
+}
 
-    return k, v
+def get_kv_cache_torch_dtype(
+    cache_dtype,
+    model_dtype = None,
+):
+    if isinstance(cache_dtype, str):
+        if cache_dtype == "auto":
+            if isinstance(model_dtype, str):
+                torch_dtype = STR_DTYPE_TO_TORCH_DTYPE[model_dtype]
+            elif isinstance(model_dtype, torch.dtype):
+                torch_dtype = model_dtype
+            else:
+                raise ValueError(f"Invalid model dtype: {model_dtype}")
+        elif cache_dtype in ["half", "bfloat16", "float"]:
+            torch_dtype = STR_DTYPE_TO_TORCH_DTYPE[cache_dtype]
+        elif cache_dtype == "fp8":
+            torch_dtype = torch.uint8
+        else:
+            raise ValueError(f"Invalid kv cache dtype: {cache_dtype}")
+    elif isinstance(cache_dtype, torch.dtype):
+        torch_dtype = cache_dtype
+    else:
+        raise ValueError(f"Invalid kv cache dtype: {cache_dtype}")
+    return torch_dtype
+
+def kv_cache_factory(
+    num_blocks: int,
+    block_size: int,
+    num_layers: int,
+    num_heads: int,
+    head_size: int,
+    cache_dtype,
+    model_dtype = None,
+    seed: int = 0,
+    device = "cuda",
+):
+
+    if cache_dtype == "fp8" and head_size % 16:
+        raise ValueError(
+            f"Does not support key cache of type fp8 with head_size {head_size}"
+        )
+
+    torch_dtype = get_kv_cache_torch_dtype(cache_dtype, model_dtype)
+
+    x = 16 // torch_dtype.itemsize
+    k_cache_shape = (num_blocks, num_heads, head_size // x, block_size, x)
+    k_caches = []
+    for _ in range(num_layers):
+        k_cache = torch.empty(size=k_cache_shape, dtype=torch_dtype, device=device)
+        if cache_dtype in ["auto", "half", "bfloat16", "float"]:
+            k_cache.uniform_(*uniform_range)
+        else:
+            raise ValueError(f"Does not support key cache of type {cache_dtype}")
+        k_caches.append(k_cache)
+
+    v_cache_shape = (num_blocks, num_heads, head_size, block_size)
+    v_caches = []
+    for _ in range(num_layers):
+        v_cache = torch.empty(size=v_cache_shape, dtype=torch_dtype, device=device)
+        if cache_dtype in ["auto", "half", "bfloat16", "float"]:
+            v_cache.uniform_(*uniform_range)
+        else:
+            raise ValueError(f"Does not support value cache of type {cache_dtype}")
+        v_caches.append(v_cache)
+    return k_caches, v_caches
+
 
 
 def create_cos_sin_cache():
@@ -77,33 +137,30 @@ def create_cos_sin_cache():
 
 
 class MLATest(TestCase):
-    SEQ_LENS = [[16]]
+    NUM_TOKENS = [16]
+    BATCH_SIZE = [64]
     HIDDEN_SIZES = [2048]
     PAGE_SIZE = [64]
+    IS_PREFILLS = [False]
+    PREFIX_LENS = [0]
 
     def setUp(self) -> None:
         if not torch.cuda.is_available():
             raise SkipTest("CUDA is not available")
         torch.set_default_device(device)
 
-    def _run_mla_test(self, sequence_lengths: list[int], hidden_size: int, page_size: int):
-        batch_size = len(sequence_lengths)
-
-        seq_page_sizes = [math.ceil(x / page_size) for x in sequence_lengths]
-        kvcache_block_id = torch.zeros(
-            [batch_size, max(seq_page_sizes)],
+    def _run_mla_test(self, num_tokens: int, batch_size: int, hidden_size: int,
+                      page_size: int, is_prefill: bool, prefix_len: int):
+        input_lengths = [num_tokens] * batch_size
+        mock_page_num = 2048
+        page_num = math.ceil(prefix_len + num_tokens + page_size - 1 / page_size)
+        block_list = [i for i in range(1, page_num + 1)]
+        # print(f"block_list: {block_list}")
+        kvcache_block_id = torch.tensor(
+            block_list,
             dtype=torch.int32,
             device=torch.device("cpu"),
         )
-        bias = 1
-        for i in range(batch_size):
-            kvcache_block_id[i, : seq_page_sizes[i]] = torch.arange(
-                bias,
-                bias + seq_page_sizes[i],
-                dtype=torch.int32,
-                device=torch.device("cpu"),
-            )
-            bias += seq_page_sizes[i]
 
         self.config = GptInitModelParameters(128, 16, 27, 1024, 102400)
         self.config.head_num = 16
@@ -119,20 +176,25 @@ class MLATest(TestCase):
         self.config.size_per_head = 192
 
         torch.manual_seed(0)
-        sequence_lengths_t = torch.tensor(
-            sequence_lengths, dtype=torch.int32, device=torch.device("cpu")
+        input_lengths_t = torch.tensor(
+            input_lengths, dtype=torch.int32, device=torch.device("cpu")
         )
-        prefix_lengths_t = torch.zeros(
-            len(sequence_lengths_t),
-            dtype=torch.int32,
-            device=torch.device("cpu"),
-        )
-        input_lengths_t = sequence_lengths_t - prefix_lengths_t
+        if is_prefill:
+            prefix_lengths = [prefix_len]*batch_size
+            prefix_lengths_t = torch.tensor(
+                prefix_lengths, dtype=torch.int32, device=torch.device("cpu")
+            )
+        else:
+            prefix_lengths_t = torch.zeros(
+                len(input_lengths), dtype=torch.int32, device=torch.device("cpu")
+            )
 
         attn_inputs: PyAttentionInputs = PyAttentionInputs()
-        attn_inputs.is_prefill = True
+        attn_inputs.is_prefill = is_prefill
         attn_inputs.prefix_lengths = prefix_lengths_t
-        attn_inputs.sequence_lengths = sequence_lengths_t
+        attn_inputs.sequence_lengths = torch.tensor(
+            [], dtype=torch.int32, device=torch.device("cpu")
+        )
         attn_inputs.input_lengths = input_lengths_t
         attn_inputs.kv_cache_block_id_host = kvcache_block_id
 
@@ -193,15 +255,27 @@ class MLATest(TestCase):
         layer_weights: List[Dict[str, torch.Tensor]] = []
         layer_weights.append(weights)
 
-        fmha_impl = AiterMlaPrefillImpl(
-            self.config, attn_inputs, layer_weights, create_cos_sin_cache()
-        )
+        if is_prefill:
+            fmha_impl = AiterMlaPrefillImpl(
+                self.config, attn_inputs, layer_weights, create_cos_sin_cache()
+            )
+        else:
+            fmha_impl = AiterMlaDecodeImpl(
+                self.config, attn_inputs, layer_weights, create_cos_sin_cache()
+            )
         deepseekv2_mla = MlaAttention(self.config, weights, 0)
-        kv_cache: Optional[KVCache] = None
+        cache = torch.randn(
+            [mock_page_num, page_size, self.config.kv_lora_rank + self.config.rope_head_dim],
+            dtype=torch.bfloat16,
+            device=device,
+        )
+        kv_cache: Optional[KVCache] = KVCache()
+        kv_cache.k_cache_base = cache
+
         deepseekv2_mla_ref = MlaAttentionRef(self.config, weights, 0)
 
         hidden = torch.randn(
-            [batch_size, self.config.hidden_size],
+            [num_tokens, self.config.hidden_size],
             dtype=torch.bfloat16,
             device=device,
         )
@@ -230,10 +304,11 @@ class MLATest(TestCase):
 
     def test_mlp(self):
         for params in itertools.product(
-            self.SEQ_LENS, self.HIDDEN_SIZES, self.PAGE_SIZE
+            self.NUM_TOKENS, self.BATCH_SIZE, self.HIDDEN_SIZES, self.PAGE_SIZE, self.IS_PREFILLS, self.PREFIX_LENS
         ):
             with self.subTest(
-                num_tokens=params[0], hidden_size=params[1], page_size=params[2]
+                    num_tokens=params[0], batch_size=params[1], hidden_size=params[2],
+                    page_size=params[3], is_prefill=params[3], prefix_len=params[4]
             ):
                 self._run_mla_test(*params)
 
